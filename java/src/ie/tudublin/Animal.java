b07fd78e99a21a0b568a5d24048807ad38f89666
pariseclipse <104399068+pariseclipse@users.noreply.github.com>
2023-02-13 23:00:24 +0000

    killing the cat

--- java/src/ie/tudublin/Animal.java
+++ java/src/ie/tudublin/Animal.java
@@ -1,27 +1,23 @@
 package ie.tudublin;
-
 public class Animal
 {
-
-    public String toString()
-    {
-        return name;
+	public static void helloProcessing()
+	{
+		String[] a = {"MAIN"};
+        processing.core.PApplet.runSketch( a, new HelloProcessing());
     }
+	public static void main(String[] args)
+	{
+		System.out.println("Hello world");
+ 		Dog penny = new Dog();
+ 		penny.setName("Penny");
+ 		penny.speak();
 
-    private String name;
+ 		Cat ginger = new Cat("Ginger");
+ 		while (ginger.getLives() > 0)
+ 		{
+ 			ginger.kill();
+ 		}
+ 	}
 
-    public String getName()
-    {
-        return name;
-    }
-
-    public void setName(String name)
-    {
-        this.name = name;
-    }
-
-    public Animal(String name)
-    {
-        this.name = name;
-    }
-}
+ }
--- java/src/ie/tudublin/Cat.java
+++ java/src/ie/tudublin/Cat.java
@@ -1,36 +1,35 @@
 package ie.tudublin;
+ public class Cat
+ {
+     private int numLives;
+     private String name;
 
-public class Cat extends Animal
-{
-    private int numLives;
+     public Cat(String name)
+     {
+         this.name = name;
+         this.numLives = 9;
+     }
 
-    public void kill()
-    {
-        if (numLives > 0)
-        {
-            System.out.println("Ouch!");
-            numLives --;
-        }
-        else
-        {
-            System.out.println("Im dead");
-        }
-    }
+     public void setLives(int numLives)
+     {
+         this.numLives = numLives;
+     }
 
-    
-    public int getNumLives() {
-        return numLives;
-    }
+     public int getLives()
+     {
+         return this.numLives;
+     }
 
-
-    public void setNumLives(int numLives) {
-        this.numLives = numLives;
-    }
-
-
-    public Cat(String name)
-    {
-        super(name);
-        numLives = 9;
-    }
-}
+     public void kill()
+     {
+         this.numLives -= 1;
+         if (this.numLives > 0)
+         {
+             System.out.println("Ouch!");
+         }
+         else
+         {
+             System.out.println("Dead");
+         }
+     }
+ }
