1112458abbe67c0afe0555301f9e432ff22ad38e
pariseclipse <104399068+pariseclipse@users.noreply.github.com>
2023-02-14 00:06:49 +0000

    bug zap game

--- java/src/ie/tudublin/BugZap.java
+++ java/src/ie/tudublin/BugZap.java
@@ -2,140 +2,212 @@ package ie.tudublin;
 
 import processing.core.PApplet;
 
-public class BugZap extends PApplet {
-	public void settings() {
-		size(1000, 1000);
+public class BugZap extends PApplet
+{
+	// Player and bug objects
+	Player player;
+	Bug bug;
+
+	// Attributes
+
+	public void settings()
+	{
+		// size(500, 500);
+		fullScreen();
 	}
 
-	public void setup() {
-		reset();
-	}
-
-	float playerX, playerY;
-	float playerSpeed = 5;
-	float playerWidth = 40;
-	float halfPlayerWidth = playerWidth / 2;
-
-	float bugX, bugY, bugWidth = 100;
-	float halfBugWidth = bugWidth / 2;
-
-	int score = 0;
-
-	void reset() {
-		resetBug();
-		playerX = width / 2;
-		playerY = height - 50;
-	}
-
-	void resetBug() {
-		bugX = random(halfBugWidth, width - halfBugWidth);
-		bugY = 50;
-	}
-
-	void drawBug(float x, float y) {
-		// Draw the bug
-		stroke(255);
-		float saucerHeight = bugWidth * 0.7f;
-		line(x, y - saucerHeight, x - halfBugWidth, y);
-		line(x, y - saucerHeight, x + halfBugWidth, y);
-		// line(x - halfBugWidth, y, x - halfBugWidth, y);
-		line(x - halfBugWidth, y, x + halfBugWidth, y);
-		float feet = bugWidth * 0.1f;
-		line(x - feet, y, x - halfBugWidth, y + halfBugWidth);
-		line(x + feet, y, x + halfBugWidth, y + halfBugWidth);
-
-		feet = bugWidth * 0.3f;
-		line(x - feet, y, x - halfBugWidth, y + halfBugWidth);
-		line(x + feet, y, x + halfBugWidth, y + halfBugWidth);
-
-		float eyes = bugWidth * 0.1f;
-		line(x - eyes, y - eyes, x - eyes, y - eyes * 2f);
-		line(x + eyes, y - eyes, x + eyes, y - eyes * 2f);
-
-	}
-
-	void drawPlayer(float x, float y, float w) {
-		stroke(255);
-		float playerHeight = w / 2;
-		line(x - halfPlayerWidth, y + playerHeight, x + halfPlayerWidth, y + playerHeight);
-		line(x - halfPlayerWidth, y + playerHeight, x - halfPlayerWidth, y + playerHeight * 0.5f);
-		line(x + halfPlayerWidth, y + playerHeight, x + halfPlayerWidth, y + playerHeight * 0.5f);
-
-		line(x - halfPlayerWidth, y + playerHeight * 0.5f, x - (halfPlayerWidth * 0.8f), y + playerHeight * 0.3f);
-		line(x + halfPlayerWidth, y + playerHeight * 0.5f, x + (halfPlayerWidth * 0.8f), y + playerHeight * 0.3f);
-
-		line(x - (halfPlayerWidth * 0.8f), y + playerHeight * 0.3f, x + (halfPlayerWidth * 0.8f),
-				y + playerHeight * 0.3f);
-
-		line(x, y, x, y + playerHeight * 0.3f);
-
-	}
-
-	public void keyPressed() {
-		
-		if (keyCode == LEFT) {
-			if (playerX > halfPlayerWidth) {
-				playerX -= playerSpeed;
-			}
-		}
-		if (keyCode == RIGHT) {
-			if (playerX < width - halfPlayerWidth) {
-				playerX += playerSpeed;
-			}
-		}
-		if (keyCode == ' ')
-		{
-			if (playerX > bugX - halfBugWidth && playerX < bugX + halfBugWidth)
-			{
-				line(playerX, playerY, playerX, bugY);
-				score ++;
-				resetBug();
-			}
-			else
-			{
-				line(playerX, playerY, playerX, 0);
-			}
-		}
-	}
-
-	void moveBug() {
-		if ((frameCount % 30) == 0) {
-			bugX += random(-5, 5);
-			if (bugX < halfBugWidth) {
-				bugX = halfBugWidth;
-			}
-			if (bugX > width - halfBugWidth) {
-				bugX = width - halfBugWidth;
-			}
-			bugY += 2;
-		}
-	}
-
-	int gameMode = 0;
-
-	public void draw() {
+	public void setup()
+	{
+		colorMode(HSB);
 		background(0);
-		fill(255);
-		text("Score: " + score, 50, 100);
-		if (gameMode == 0)
+		this.player = new Player();
+		this.bug = new Bug();
+
+		smooth();
+	}
+
+	public void draw()
+	{
+		background(0);
+
+		// drawplayer(playerX, playerY, playerWidth);
+
+		if (frameCount % 60 == 0)
+		{
+			this.bug.move();
+		}
+
+		if (this.bug.getY() > height)
+		{
+			this.bug.resetBug();
+		}
+
+		this.player.draw();
+		this.bug.draw();
+
+	}
+
+	public void keyPressed()
+	{
+		if (keyCode == LEFT)
+		{
+			System.out.println("Left");
+			player.moveLeft();
+		}
+		if (keyCode == RIGHT)
+		{
+			System.out.println("Right");
+			player.moveRight();
+		}
+		if (key == ' ')
+		{
+			System.out.println("Space");
+			player.shoot(bug);
+		}
+	}
+
+
+	class GameEntity
+	{
+		// Attributes
+		private float x;
+		private float y;
+		private float w;
+		private float h;
+
+		// Constructor
+		public GameEntity(int x, int y, int w, int h)
+		{
+			this.x = x;
+			this.y = y;
+			this.w = w;
+			this.h = h;
+		}
+
+		public float getX() { return this.x; }
+
+		public float getY() { return this.y; }
+
+		public float getW() { return this.w; }
+
+		public float getH() { return this.h; }
+
+		public void moveX(float dx)
+		{
+			if (this.x + dx < 0)
+			{
+				this.x = 0;
+				return;
+			}
+			if (this.x + dx > width)
+			{
+				this.x = width;
+				return;
+			}
+			this.x += dx;
+		}
+
+		public void moveY(float dy)
+		{
+			if (this.y + dy < 0)
+			{
+				this.y = 0;
+				return;
+			}
+			if (this.y + dy > height)
+			{
+				this.y = height;
+				return;
+			}
+			this.y += dy;
+		}
+	}
+
+	class Player extends GameEntity
+	{
+		float speed = 10;
+		public Player()
+		{
+			super(width/2, height - 100, 100, 50);
+		}
+
+		public void resetPlayer()
+		{
+			super.x = width / 2;
+			super.y = height - 100;
+		}
+
+		public void moveLeft()
+		{
+			super.moveX(-speed);
+		}
+		public void moveRight()
+		{
+			super.moveX(speed);
+		}
+
+		public void shoot(Bug bug)
+		{
+			stroke(255);
+			line(super.x, super.y, super.x, 0);
+			if (bug.isHit(super.x))
+			{
+				bug.resetBug();
+			}
+		}
+
+		public void draw()
 		{
 			fill(255);
-			drawPlayer(playerX, playerY, playerWidth);
-			drawBug(bugX, bugY);
-			moveBug();
+			stroke(255);
+			strokeWeight(2);
 
-			text("Score: " + score, 20, 20);
+			// Origin of graphics is top left
+			float originX = super.x - super.w/2;
+			float originY = super.y - super.h/2;
+
+			line(originX, originY, originX + super.w, originY);
 		}
-		else
+	}
+
+	class Bug extends GameEntity
+	{
+		float speed = 10;
+
+		public Bug()
 		{
-			textAlign(CENTER, CENTER);
-			text("GAME OVER!!!", width / 2, height / 2);
+			super(width/2, 0, 100, 50);
 		}
-
-		if (bugY > height - 50)
+		public void resetBug()
 		{
-			gameMode = 1;
+			super.x = random(0, width);
+			super.y = 0;
 		}
 
+		public void move()
+		{
+			super.moveX(random(-100, 100));
+			super.moveY(100);
+		}
+
+		public boolean isHit(float x)
+		{
+			return x > super.x - super.w/2 && x < super.x + super.w/2;
+		}
+
+		public void draw()
+		{
+			fill(255);
+			stroke(255);
+			strokeWeight(2);
+
+			// Origin of graphics is top left
+			float originX = super.x - super.w/2;
+			float originY = super.y - super.h/2;
+
+			// triangle(super.x - super.w/2, super.y - super.h/2, super.x + super.w/2, super.y - super.h/2, super.x, super.y + super.h/2);
+			triangle(originX, originY, originX + super.w, originY, originX + super.w/2, originY + super.h);
+
+		}
 	}
 }
--- java/src/ie/tudublin/Main.java
+++ java/src/ie/tudublin/Main.java
@@ -9,11 +9,6 @@ public class Main
         processing.core.PApplet.runSketch( a, new HelloProcessing());
     }
 
-	public static void loops()
-	{
-		String[] a = {"MAIN"};
-        processing.core.PApplet.runSketch( a, new Loops());
-    }
 
 	public static void bugZap()
 	{
@@ -21,21 +16,15 @@ public class Main
         processing.core.PApplet.runSketch( a, new BugZap());
     }
 
-	public static void starMap()
-	{
-		String[] a = {"MAIN"};
-        processing.core.PApplet.runSketch( a, new StarMap());
-    }
-	
 	public static void main(String[] args)
 	{
 		System.out.println("Hello world");
-		
+
 		Dog penny = new Dog();
 		penny.setName("Penny");
 		penny.speak();
 
-		starMap();
+		bugZap();
 	}
-	
+
 }
